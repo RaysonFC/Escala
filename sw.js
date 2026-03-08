77b03f5967beb4b31940528c1cae1401377d5e1e
RAYSON FERREIRA CRUZ <167266574+RaysonFC@users.noreply.github.com>
2026-03-08 15:28:21 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,43 +1,8 @@
-const CACHE = 'banda-aviva-v4';
-const ASSETS = [
-  '/Escala/',
-  '/Escala/index.html',
-  '/Escala/manifest.json',
-  '/Escala/icon.png',
-  'https://unpkg.com/react@18/umd/react.production.min.js',
-  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
-  'https://unpkg.com/@babel/standalone/babel.min.js',
-  'https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js',
-  'https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore-compat.js',
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(ASSETS)).catch(()=>{})
-  );
-  self.skipWaiting();
-});
-
+// Desinstala todos os caches antigos
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
   );
   self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  if(e.request.url.includes('firestore') || 
-     e.request.url.includes('googleapis') ||
-     e.request.url.includes('youtube')) return;
-
-  // Sempre busca na rede primeiro, só usa cache se falhar
-  e.respondWith(
-    fetch(e.request).then(res => {
-      const clone = res.clone();
-      caches.open(CACHE).then(cache => cache.put(e.request, clone));
-      return res;
-    }).catch(() => caches.match(e.request))
-  );
 });
